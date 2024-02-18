5a164887c003d8424f707e6a0097265bbf9bdf74
salka <salkash04example.com@outlook.com>
2024-02-18 21:16:35 +0300

    add helm

---
+++ src/test/java/ru/myproject/helmproject/HelmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.myproject.helmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
